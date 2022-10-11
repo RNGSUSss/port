b97d61f1b88a74a7c513620445d79f386c1c8bbd
RNGSUSss <78634323+RNGSUSss@users.noreply.github.com>
2022-10-11 23:11:16 +0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,10 @@
+window.onload = loadPage;
+function loadPage()
+{
+    var ass4 = document.getElementById("ass4")
+    ass4.onclick = as4LoadPage
+}
+function as4LoadPage()
+{
+    location.href= "https://rngsusss.github.io/stupid-guy-with-stupid-code/";
+}
